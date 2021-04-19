5633af5fbca15c1f8c28124eb5616d421bacb453
Tobias <tobwiens@gmail.com>
2021-04-20 00:36:22 +0200

    Remove commented out code

--- k8s-provider.tf
+++ k8s-provider.tf
@@ -1,26 +1,14 @@
 data "google_client_config" "provider" {}
 
 
-
 provider "kubernetes" {
 
   host = google_container_cluster.gke_cluster.endpoint
 
 
-  client_certificate =  base64decode(google_container_cluster.gke_cluster.master_auth.0.client_certificate)
-  client_key =  base64decode(google_container_cluster.gke_cluster.master_auth.0.client_key)
+  client_certificate = base64decode(google_container_cluster.gke_cluster.master_auth.0.client_certificate)
+  client_key = base64decode(google_container_cluster.gke_cluster.master_auth.0.client_key)
   token = data.google_client_config.provider.access_token
   cluster_ca_certificate = base64decode(google_container_cluster.gke_cluster.master_auth.0.cluster_ca_certificate)
 }
 
-//resource "kubernetes_cluster_role" "client_role" {
-//  metadata {
-//    name = "terraform-example"
-//  }
-//
-//  rule {
-//    api_groups = [""]
-//    resources  = ["namespaces", "pods"]
-//    verbs      = ["get", "list", "watch"]
-//  }
-//}
